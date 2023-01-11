8473d91ab9123442c8d9248c1f2963b8238fa016
STIXGT <stiveguano@gmail.com>
2023-01-10 20:00:20 -0500

    Tarea terminada

--- src/ec/edu/puce/MainFrame.java
+++ src/ec/edu/puce/MainFrame.java
@@ -52,19 +52,27 @@ public class MainFrame extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Nombre");
-		lblNewLabel.setBounds(12, 12, 70, 15);
-		contentPane.add(lblNewLabel);
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(12, 12, 70, 15);
+		contentPane.add(lblNombre);
 
 		JLabel lblApellido = new JLabel("Apellido");
 		lblApellido.setBounds(12, 39, 70, 15);
 		contentPane.add(lblApellido);
 
-		JLabel lblDireccin = new JLabel("Dirección");
-		lblDireccin.setBounds(12, 77, 70, 15);
-		contentPane.add(lblDireccin);
+		JLabel lblDireccion = new JLabel("Dirección");
+		lblDireccion.setBounds(12, 77, 70, 15);
+		contentPane.add(lblDireccion);
 
 		JButton btnNuevo = new JButton("Nuevo");
+		btnNuevo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtNombre.setText(null);
+				txtApellido.setText(null);
+				txtDireccion.setText(null);
+				lblMensaje.setText(null);
+			}
+		});
 		btnNuevo.setBounds(65, 214, 117, 25);
 		contentPane.add(btnNuevo);
 
@@ -78,6 +86,11 @@ public class MainFrame extends JFrame {
 		contentPane.add(btnGuardar);
 
 		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
 		btnCancelar.setBounds(323, 214, 117, 25);
 		contentPane.add(btnCancelar);
 
@@ -87,17 +100,33 @@ public class MainFrame extends JFrame {
 			public void focusLost(FocusEvent e) {
 				txtNombre.setText(txtNombre.getText().toUpperCase());
 			}
+
 		});
+
+		txtApellido = new JTextField();
+		txtApellido.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				txtApellido.setText(txtApellido.getText().toUpperCase());
+			}
+		});
+
+		txtDireccion = new JTextField();
+		txtDireccion.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent e) {
+				txtDireccion.setText(txtDireccion.getText().toUpperCase());
+			}
+		});
+
 		txtNombre.setBounds(100, 10, 114, 19);
 		contentPane.add(txtNombre);
 		txtNombre.setColumns(10);
 
-		txtApellido = new JTextField();
 		txtApellido.setBounds(100, 37, 114, 19);
 		contentPane.add(txtApellido);
 		txtApellido.setColumns(10);
 
-		txtDireccion = new JTextField();
 		txtDireccion.setBounds(100, 75, 114, 19);
 		contentPane.add(txtDireccion);
 		txtDireccion.setColumns(10);
